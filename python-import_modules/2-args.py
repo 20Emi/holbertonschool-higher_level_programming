eeb83fa3a59b30a29da1665b9f093c436f31ea59
20Emi <iop.emilysanchez1@gmail.com>
2023-06-02 07:21:33 -0700

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if argument == 0:
         print("0 arguments.")
     elif argument == 1:
-        print("1 argument:")
+        print("1 arguments:")
     else:
         print("{} arguments:".format(argument)) 
     for a in range(1, argument + 1):
