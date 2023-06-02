ec98c7c4e953fc28635d4e65b28b860c9d7ec7ac
Emily Sánchez <124268926+20Emi@users.noreply.github.com>
2023-06-02 13:46:36 -0300

    Delete python-if_else_loops_functions directory

--- python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-for num in range(0, 10):
-    for num1 in range((1+num), 10):
-        if num == 0 and num1 == 1:
-            print("{}{}".format(num, num1), end='')
-        elif num1 != num:
-            print(", {}{}".format(num, num1), end='')
-print()
