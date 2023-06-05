241f2df123f8c3789e2f77ccdb75d73b7535d0ef
20Emi <iop.emilysanchez1@gmail.com>
2023-06-05 10:27:43 -0700

    task6 remove

--- python-data_structures/6-print_matrix_integer.py
+++
@@ -1,9 +0,0 @@
-#!/urs/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for mem1 in range(len(matrix)):
-        for mem in range(len(matrix[mem1])):
-            if mem != len(matrix[mem]) - 1:
-                print("{:d} ".format(matrix[mem1][mem]), end='')
-            else:
-                print("{:d}".format(matrix[mem1][mem]), end='')
-        print()
