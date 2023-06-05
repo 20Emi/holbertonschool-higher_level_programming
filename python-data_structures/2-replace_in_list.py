a240ea9bc20faf224c76eaec2c706a2fe59a25f7
20Emi <iop.emilysanchez1@gmail.com>
2023-06-05 07:03:22 -0700

    task competed

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    my_list[idx] = element
-    if idx < 0 or len(my_list) - 1 < idx:
-        return my_list
 
+    my_list[idx] = element
+    if idx < 0:
+        return my_list
+    if len(my_list) - 1 < idx:
+        return my_list
+    
     return my_list
