44b7b885dab522e84346415ac13d3c48875fd1b4
20Emi <iop.emilysanchez1@gmail.com>
2023-06-07 06:45:27 -0700

    new director's name

---
+++ 04-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+
+    if len(my_list) == 0:
+        "None"
+
+    mx = my_list[0]
+    for a in range(len(my_list)):
+        if mx < my_list[a]:
+            mx = my_list[a]
+    return mx
