0d4ae0dcac9d443008bc213b7f1ed0e97d34420c
20Emi <iop.emilysanchez1@gmail.com>
2023-06-13 07:08:01 -0700

    task 6.

---
+++ python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""task 6"""
+
+
+class Square:
+    """Private instance attribute: size and position"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        else:
+            self.__position = value
+
+    def area(self):
+        """Public instance method:"""
+        return self.__size * self.__size
+
+    def my_print(self):
+
+        if self.__size == 0:
+            print()
+        for s in range(self.__position[1]):
+            print()
+        for a in range(self.__size):
+            for d in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
