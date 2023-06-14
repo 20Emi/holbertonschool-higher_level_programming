6f92e2d6e1765f2d25266ab853d57aeb4c4ce1b0
20Emi <iop.emilysanchez1@gmail.com>
2023-06-14 07:48:40 -0700

    update.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""task 0"""
+
+
 def add_integer(a, b=98):
+    """adds"""
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
