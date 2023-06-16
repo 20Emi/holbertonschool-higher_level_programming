32c938b81b8aad59b3c39600a8b0988e16cc8d14
20Emi <iop.emilysanchez1@gmail.com>
2023-06-16 10:37:28 -0700

    update

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -11,8 +11,7 @@ def print_square(size):
             raise TypeError("size must be an integer")
     if size < 0:
             raise ValueError("size must be >= 0")
-    for a in range(size):
-        
+    for a in range(size): 
         for j in range(size):
             print("#", end="")
         print()
