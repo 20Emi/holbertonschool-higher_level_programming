0832e3cdee7c12eae763fc630fcb0944a5a7b78a
20Emi <iop.emilysanchez1@gmail.com>
2023-06-17 16:36:55 -0700

    update directory

---
+++ 07-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""task 1"""
+
+
+class Square:
+    """*Private instance attribute: size
+        *Instantiation with size"""
+
+    def __init__(self, size):
+        self.__size = size
---
+++ 07-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""task 5"""
+
+
+class Square:
+    """Private instance attribute: size"""
+
+    def __init__(self, size=0):
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Public instance method:"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for a in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
