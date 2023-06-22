e1d29fa10048ba2a514aad24042134e816594f2d
20Emi <iop.emilysanchez1@gmail.com>
2023-06-22 13:29:00 -0700

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 """Task 7"""
+
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file,
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
+
+    nomb = "add_item.json"
+
+    try:
+        js = load_from_json_file(nomb)
+    except FileNotFoundError:
+        js = []
+        js.extend(sys.argv[1:])
+        """'extend()' is used to add multiple items to an existing list."""
+        save_to_json_file(js, nomb)
