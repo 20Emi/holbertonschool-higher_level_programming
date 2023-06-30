7293dd0e9ce907a343038531e9b2ee5b2d23139c
20Emi <iop.emilysanchez1@gmail.com>
2023-06-30 09:56:20 -0700

    task

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""""""
+
+import unittest
+from models.base import Base
+
+
+class Test_Base(unittest.TestCase):
+    def test_class(self):
+        bese1 = base()
+        self.assertAlmostEqual(base1.id)
