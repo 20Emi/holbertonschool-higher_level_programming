49dc0f540761dd29f2ee722538d224537543ad09
20Emi <iop.emilysanchez1@gmail.com>
2023-07-27 13:40:31 -0700

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""task 2"""
+import sys  # para los argumentos
+import MySQLdb
+
+if __name__ == "__main__":
+    # conexion con la base de datos
+    conex = MySQLdb.connect(host="localhost",
+                            user=sys.argv[1],
+                            password=sys.argv[2],
+                            database=sys.argv[3])
+
+    # Creacion del cursor
+    cursor = conex.cursor()
+
+    # ejecucion
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in cursor.fetchall():
+        if row[1] == sys.argv[4]:
+            print("{}".format(row))
+
+    # cierre del cursor
+    conex.close()
