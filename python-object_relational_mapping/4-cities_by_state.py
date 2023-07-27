ce3c2c42aea813c44471f4f3b31e8388b5c4bb5f
20Emi <iop.emilysanchez1@gmail.com>
2023-07-27 14:48:26 -0700

    task sql

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""task 2"""
+import sys  # para los argumentos
+import MySQLdb
+
+if __name__ == "__main__":
+    # conexion con la base de datos
+    conex = MySQLdb.connect(host="localhost",
+                            user=sys.argv[1],
+                            password=sys.argv[2],
+                            database=sys.argv[3])
+
+    # Creacion del cursor
+    cursor = conex.cursor()
+
+    # ejecucion
+    sql_query = """SELECT cities.id, cities.name, states.name FROM states
+    INNER JOIN cities ON cities.state_id = states.id
+    ORDER BY id"""
+    cursor.execute(sql_query)
+
+    for row in cursor.fetchall():
+        print(row)
+
+    # cierre del cursor
+    conex.close()
