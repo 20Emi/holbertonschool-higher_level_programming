ba2d9d21166810fde2fa1079b6c5b95836895ec5
20Emi <iop.emilysanchez1@gmail.com>
2023-07-27 14:02:42 -0700

    task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""task 2"""
+import sys  # para los argumentos
+import MySQLdb
+
+if __name__ == "__main__":
+    # conexion con la base de datos
+    conex = MySQLdb.connect(host="localhost",
+                            user=sys.argv[1],
+                            password=sys.argv[2],
+                            database=sys.argv[3])
+
+    # Creacion del cursor
+    cursor = conex.cursor()
+
+    # ejecucion
+    sql_query = "SELECT * FROM states ORDER BY id"
+    cursor.execute(sql_query)
+
+    SQL = sys.argv[4]
+    for row in cursor.fetchall():
+        if row[1] == SQL:
+            print("{}".format(row))
+
+    # cierre del cursor
+    conex.close()
