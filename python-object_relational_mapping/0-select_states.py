d7135f20c2aa7af282c492fb558fb644a6449a04
20Emi <iop.emilysanchez1@gmail.com>
2023-07-27 12:23:29 -0700

    task

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys  # para los argumentos
+import MySQLdb
+
+if __name__ == "__main__":
+    # conexion con la base de datos
+    conex = MySQLdb.connect(host="localhost",
+                            user=sys.argv[1],
+                            password=sys.argv[2],
+                            database=sys.argv[3])
+
+    # Creacion del cursor
+    cursor = conex.cursor()
+
+    # ejecucion
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    # cierre del cursor
+    conex.close()
