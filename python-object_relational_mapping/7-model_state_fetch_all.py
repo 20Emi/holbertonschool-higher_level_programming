58533695c4c32a6553c87daed8175dc3c8f6f21b
20Emi <iop.emilysanchez1@gmail.com>
2023-07-31 09:08:18 -0700

    task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+
+
+import sys
+import sqlalchemy
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    """task 7"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    conexion = engine.connect()
+    la = sqlalchemy.select(State.id, State.name).order_by(State.id)
+    ls = conexion.execute(la).fetchall()
+    for a in ls:
+        print("{}:{}".format(a[0], a[1]))
